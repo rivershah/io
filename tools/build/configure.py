dc1256966b063af17aa4c61b5bab5efa44c824f1
rivershah <Ansar.sa@gmail.com>
2024-11-26 16:11:14 +0000

    update cuda

--- tools/build/configure.py
+++ tools/build/configure.py
@@ -105,14 +105,10 @@ def write_config():
             for argv in sys.argv[1:]:
                 if argv == "--cuda":
                     bazel_rc.write('build --action_env TF_NEED_CUDA="1"\n')
-                    bazel_rc.write(
-                        'build --action_env CUDA_TOOLKIT_PATH="/usr/local/cuda"\n'
-                    )
-                    bazel_rc.write(
-                        'build --action_env CUDNN_INSTALL_PATH="/usr/lib/x86_64-linux-gnu"\n'
-                    )
-                    bazel_rc.write('build --action_env TF_CUDA_VERSION="10.1"\n')
-                    bazel_rc.write('build --action_env TF_CUDNN_VERSION="7"\n')
+                    bazel_rc.write('build --action_env CUDA_TOOLKIT_PATH="/usr/local/cuda"\n')
+                    bazel_rc.write('build --action_env CUDNN_INSTALL_PATH="/usr/lib/x86_64-linux-gnu"\n')
+                    bazel_rc.write('build --action_env TF_CUDA_VERSION="12.3"\n')
+                    bazel_rc.write('build --action_env TF_CUDNN_VERSION="8"\n')
             # Needed for tf rules
             bazel_rc.write("build --experimental_repo_remote_exec\n")
             # Enable platform specific config
